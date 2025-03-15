7256670dc2c7e1304c7f3a36725aca816c54d466
Vladyslav Lysenko <vlad.lysenko1999@gmail.com>
2025-03-15 14:48:52 +0200

    Add initial project files and structure for DesktopApp with database integration

---
+++ DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SqlClient;
+using System.Windows;
+using DatabaseService.DBProvider;
+
+namespace DesktopApp;
+
+public partial class MainWindow : Window
+{
+    private DatabaseProvider _provider;
+    public MainWindow()
+    {
+        InitializeComponent();
+        var connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=master;Trusted_Connection=True;";
+        _provider = new DatabaseProvider(connectionString);
+    }
+
+    [Obsolete("Obsolete")]
+    private async void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await _provider.InitializeDatabaseAsync();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+            await _provider.ResetDatabaseAsync();
+            await _provider.InitializeDatabaseAsync();
+        }
+        finally
+        {
+            MainWindowFrame.Navigate(new Pages.LoginPage(_provider));
+        }
+    }
+}
+
